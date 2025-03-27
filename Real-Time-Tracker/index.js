a053b0f62c6b086ce8197f0fc1d9a786a758be4f
Mimansa Sahay <mimansa192@gmail.com>
2025-03-27 12:41:55 +0530

    Added basic features

---
+++ Real-Time-Tracker/index.js
@@ -0,0 +1,35 @@
+import express from "express"
+import http from "http"
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+import { log } from "console";
+const app=express()
+
+const server=http.createServer(app)  //main method
+const io = new Server(server);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.set("view engine","ejs")
+app.set('views', './views');
+app.use(express.static(path.join(__dirname,"public")))
+
+io.on("connection",function(socket){   //cb   //in cb we pass unique socket key(socket is unique) //io is basically server by socketio server  //setting server
+    socket.on("send-location",function(data){   //this is the what we accepting from frontend-send location
+        io.emit("recieved-location",{    //now backend will send location to all connected ppl  i.e to frontend
+            id:socket.id,   //every person have unique socket id
+           ...data         //send all data recived lat long
+         })  
+
+    })
+    console.log("connected")
+})
+
+app.get('/',(req,res)=>{
+    res.render("index")   //res.send('Hello World')  //rendering ejs file
+})
+
+server.listen(3000);
---
+++ Real-Time-Tracker/public/js/script.js
@@ -0,0 +1,34 @@
+const socket=io()  //this send a connection request to backend
+
+if(navigator.geolocation){  //navigator is inbuilt in window obj
+    navigator.geolocation.watchPosition((position)=>{                //whenver guy moves watch its position  //position is what is the actul value of its position(lat)
+        const {latitude,longitude}=position.coords;              //extract position coords and send in backend
+        socket.emit("send-location",{latitude,longitude})       //an event??
+    },(error)=>{
+        console.log(error)
+    },{
+        enableHighAccuracy:true,  //true means it will give you the exact location
+        maximumAge:0, //no caching of the guy //dont give saved data ,actual real time data dena hai
+        timeout:5000   //recheck after every 5 sec location
+    })        
+
+}
+
+const map=L.map("map").setView([0,0],10)      //L.map is give your loaction access,(0,0 ) map is centered at this by setting coordinates(lat,long) of earth and 10 is zoom level
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "openstreetmap"
+}).addTo(map)
+
+const markers={};
+
+socket.on("recieved-location", (data) => {   // we got location again now from backend
+    const { id, latitude, longitude } = data;   //extract data
+    map.setView([latitude, longitude], 16);          //it will like centre the map to that lat/lang with zoom level 16
+    if (markers[id]) {                                //if the user with this id already exists then update its lat/lang
+        markers[id].setLatLng([latitude, longitude]);
+    }
+    else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);    //else set a new marker at that lat/lang and add map is adding to map
+    }
+});
